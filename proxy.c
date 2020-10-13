ebad1cfe0cebc5b97cf79e6347f65144d5b1d6f4
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-10-13 20:58:15 +0800

    new

--- proxy.c
+++ proxy.c
@@ -1,14 +1,29 @@
-#include <stdio.h>
+#include "csapp.h"
 
 /* Recommended max cache and object sizes */
+#define HOSTNAME_MAX_LEN 63
+#define PORT_MAX_LEN 10
+#define HEADER_NAME_MAX_LEN 32
+#define HEADER_VALUE_MAX_LEN 64
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
-/* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static const char *user_agent_hdr =
+    "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
+    "Firefox/10.0.3\r\n";
 
-int main()
-{
-    printf("%s", user_agent_hdr);
-    return 0;
+typedef struct {
+  char host[HOSTNAME_MAX_LEN];
+  char port[PORT_MAX_LEN];
+  char path[MAXLINE];
+} RequestLine;
+
+typedef struct {
+  char name[HEADER_NAME_MAX_LEN];
+  char value[HEADER_VALUE_MAX_LEN];
+} RequestHeader;
+
+int main() {
+  printf("%s", user_agent_hdr);
+  return 0;
 }
