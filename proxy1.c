d1f723ec9d0cdb158f1b31ea7801502b42cd9825
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-10-14 19:07:16 +0800

    fix

--- proxy1.c
+++ proxy1.c
@@ -1,25 +1,29 @@
 #include "csapp.h"
 
+/* Recommended max cache and object sizes */
 #define HOSTNAME_MAX_LEN 63
 #define PORT_MAX_LEN 10
 #define HEADER_NAME_MAX_LEN 32
 #define HEADER_VALUE_MAX_LEN 64
-#define MAX_CHACHE_SIZE (1 << 20)
+#define MAX_CACHE_SIZE (1 << 20)
 #define MAX_OBJECT_SIZE ((1 << 10) * 100)
 
-/**
- * data structures
- */
+/*
+static const char *user_agent_hdr =
+    "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
+    "Firefox/10.0.3\r\n";
+*/
+
 typedef struct {
   char host[HOSTNAME_MAX_LEN];
   char port[PORT_MAX_LEN];
   char path[MAXLINE];
-} ReqLine;
+} RequestLine;
 
 typedef struct {
   char name[HEADER_NAME_MAX_LEN];
   char value[HEADER_VALUE_MAX_LEN];
-} ReqHeader;
+} RequestHeader;
 
 typedef struct {
   char *name;
@@ -31,22 +35,18 @@ typedef struct {
   CacheLine *objects;
 } Cache;
 
-/**
- * pre-declaration of functions
- */
 void doit(int fd);
-void parse_request(int fd, ReqLine *linep, ReqHeader *headers, int *num_hds);
-void parse_uri(char *uri, ReqLine *linep);
-ReqHeader parse_header(char *line);
-int send_to_server(ReqLine *line, ReqHeader *headers, int num_hds);
+void parse_request(int fd, RequestLine *request_line, RequestHeader *headers,
+                   int *num_hds);
+void parse_uri(char *uri, RequestLine *request_line);
+RequestHeader parse_header(char *line);
+int send_to_server(RequestLine *request_line, RequestHeader *headers,
+                   int num_hds);
 void *thread(void *vargp);
 void init_cache();
 int reader(int fd, char *uri);
 void writer(char *uri, char *buf);
 
-/**
- * reader-writer model
- */
 Cache cache;
 int readcnt;
 sem_t mutex, w;
@@ -76,21 +76,9 @@ int main(int argc, char **argv) {
   }
 }
 
-void init_cache() {
-  Sem_init(&mutex, 0, 1);
-  Sem_init(&w, 0, 1);
-  readcnt = 0;
-  cache.objects = (CacheLine *)Malloc(sizeof(CacheLine) * 10);
-  cache.used_cnt = 0;
-  for (int i = 0; i < 10; ++i) {
-    cache.objects[i].name = Malloc(sizeof(char) * MAXLINE);
-    cache.objects[i].object = Malloc(sizeof(char) * MAX_OBJECT_SIZE);
-  }
-}
-
 void *thread(void *vargp) {
   int connfd = *((int *)vargp);
-  Pthread_detach(pthread_self()); //将线程分离，让这个线程计数结束后自己回收资源
+  Pthread_detach(pthread_self());
   Free(vargp);
   doit(connfd);
   Close(connfd);
@@ -101,8 +89,8 @@ void doit(int fd) {
   char buf[MAXLINE], uri[MAXLINE], object_buf[MAX_OBJECT_SIZE];
   int total_size, connfd;
   rio_t rio;
-  ReqLine request_line;
-  ReqHeader headers[20];
+  RequestLine request_line;
+  RequestHeader headers[20];
   int num_hds, n;
   parse_request(fd, &request_line, headers, &num_hds);
 
@@ -122,46 +110,13 @@ void doit(int fd) {
     strcpy(object_buf + total_size, buf);
     total_size += n;
   }
-  if (total_size < MAX_OBJECT_SIZE) {
+  if (total_size < MAX_OBJECT_SIZE)
     writer(uri, object_buf);
-  }
   Close(connfd);
 }
 
-int reader(int fd, char *uri) {
-  int in_cache = 0;
-  P(&mutex);
-  readcnt++;
-  if (readcnt == 1) {
-    P(&w);
-  }
-  V(&mutex);
-
-  for (int i = 0; i < 10; ++i) {
-    if (!strcmp(cache.objects[i].name, uri)) {
-      Rio_writen(fd, cache.objects[i].object, MAX_OBJECT_SIZE);
-      in_cache = 1;
-      break;
-    }
-  }
-  P(&mutex);
-  readcnt--;
-  if (readcnt == 0) {
-    V(&w);
-  }
-  V(&mutex);
-  return in_cache;
-}
-
-void writer(char *uri, char *buf) {
-  P(&w);
-  strcpy(cache.objects[cache.used_cnt].name, uri);
-  strcpy(cache.objects[cache.used_cnt].object, buf);
-  ++cache.used_cnt;
-  V(&w);
-}
-
-void parse_request(int fd, ReqLine *linep, ReqHeader *headers, int *num_hds) {
+void parse_request(int fd, RequestLine *request_line, RequestHeader *headers,
+                   int *num_hds) {
   char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
   rio_t rio;
 
@@ -169,11 +124,9 @@ void parse_request(int fd, ReqLine *linep, ReqHeader *headers, int *num_hds) {
   if (!Rio_readlineb(&rio, buf, MAXLINE))
     return;
 
-  // parse request line
   sscanf(buf, "%s %s %s", method, uri, version);
-  parse_uri(uri, linep);
+  parse_uri(uri, request_line);
 
-  // parse request headers
   *num_hds = 0;
   Rio_readlineb(&rio, buf, MAXLINE);
   while (strcmp(buf, "\r\n")) {
@@ -182,7 +135,7 @@ void parse_request(int fd, ReqLine *linep, ReqHeader *headers, int *num_hds) {
   }
 }
 
-void parse_uri(char *uri, ReqLine *linep) {
+void parse_uri(char *uri, RequestLine *request_line) {
   if (strstr(uri, "http://") != uri) {
     fprintf(stderr, "Error: invalid uri!\n");
     exit(0);
@@ -190,18 +143,18 @@ void parse_uri(char *uri, ReqLine *linep) {
   uri += strlen("http://");
   char *c = strstr(uri, ":");
   *c = '\0';
-  strcpy(linep->host, uri);
+  strcpy(request_line->host, uri);
   uri = c + 1;
   c = strstr(uri, "/");
   *c = '\0';
-  strcpy(linep->port, uri);
+  strcpy(request_line->port, uri);
   *c = '/';
-  strcpy(linep->path, c);
+  strcpy(request_line->path, c);
 }
 
-ReqHeader parse_header(char *line) {
-  ReqHeader header;
-  char *c = strstr(line, ": ");
+RequestHeader parse_header(char *line) {
+  RequestHeader header;
+  char *c = strstr(line, ":");
   if (c == NULL) {
     fprintf(stderr, "Error: invalid header: %s\n", line);
     exit(0);
@@ -212,7 +165,7 @@ ReqHeader parse_header(char *line) {
   return header;
 }
 
-int send_to_server(ReqLine *line, ReqHeader *headers, int num_hds) {
+int send_to_server(RequestLine *line, RequestHeader *header, int num_hds) {
   int clientfd;
   char buf[MAXLINE], *buf_head = buf;
   rio_t rio;
@@ -222,11 +175,55 @@ int send_to_server(ReqLine *line, ReqHeader *headers, int num_hds) {
   sprintf(buf_head, "GET %s HTTP/1.0\r\n", line->path);
   buf_head = buf + strlen(buf);
   for (int i = 0; i < num_hds; ++i) {
-    sprintf(buf_head, "%s: %s", headers[i].name, headers[i].value);
+    sprintf(buf_head, "%s : %s", header[i].name, header[i].value);
     buf_head = buf + strlen(buf);
   }
   sprintf(buf_head, "\r\n");
   Rio_writen(clientfd, buf, MAXLINE);
-
   return clientfd;
+}
+
+void init_cache() {
+  Sem_init(&mutex, 0, 1);
+  Sem_init(&w, 0, 1);
+  readcnt = 0;
+  cache.objects = (CacheLine *)Malloc(sizeof(CacheLine) * 10);
+  cache.used_cnt = 0;
+  for (int i = 0; i < 10; ++i) {
+    cache.objects[i].name = Malloc(sizeof(char) * MAXLINE);
+    cache.objects[i].object = Malloc(sizeof(char) * MAX_OBJECT_SIZE);
+  }
+}
+
+int reader(int fd, char *uri) {
+  int in_cache = 0;
+  P(&mutex);
+  ++readcnt;
+  if (readcnt == 1)
+    P(&w);
+  V(&mutex);
+  // Critical Section Begin
+  for (int i = 0; i < 10; ++i) {
+    if (!strcmp(cache.objects[i].name, uri)) {
+      Rio_writen(fd, cache.objects[i].object, MAX_OBJECT_SIZE);
+      in_cache = 1;
+      break;
+    }
+  }
+  // Critical Section End
+  P(&mutex);
+  --readcnt;
+  if (readcnt == 0)
+    V(&w);
+  V(&mutex);
+  return in_cache;
+}
+
+void writer(char *uri, char *buf) {
+  P(&w);
+  // Critical Section Begin
+  strcpy(cache.objects[cache.used_cnt].name, uri);
+  strcpy(cache.objects[cache.used_cnt].object, buf);
+  ++cache.used_cnt;
+  V(&w);
 }
